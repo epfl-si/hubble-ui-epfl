fbb20db17e5fbf32d23ffe42cf78bcf349ecc7c0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-05-06 20:56:51 +0200

    [refactor] Hoist `onParentIframeMessage` into `src/iframe-api/index.ts`
    While we are there, provide `useEffect`-friendly cleanup functionality

--- src/iframe-api/index.ts
+++ src/iframe-api/index.ts
@@ -8,3 +8,20 @@ type BearerToken = {
   kind: "token",
   token: string
 }
+
+/**
+ * @return A void function that unregisters the handler when called.
+ */
+export function onParentIframeMessage (handler : (message : ReceivedMessage) => void)
+: () => void {
+  function processEvent (event : MessageEvent) {
+    if (event.source !== window.parent) {
+      console.error("OMG H4XX !!1!");
+      return;
+    }
+    handler(event.data);
+  }
+  window.addEventListener("message", processEvent);
+
+  return () => window.removeEventListener("message", processEvent);
+}

6d6cb1e247503847d79c3a5c483a8b463aae8b35
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-05-06 12:50:49 +0200

    [feature] Draggable blank space to the left and the top of the iframe
    Uses [`split.js`](https://split.js.org/)

--- devsupport/iframe-parent/iframe-parent.tsx
+++ devsupport/iframe-parent/iframe-parent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Split from 'react-split';
+import "./split.css";
 
 const div = document.getElementById("hubble-ui-iframed") as Element;
 createRoot(div).render(<React.StrictMode>
@@ -7,6 +9,22 @@ createRoot(div).render(<React.StrictMode>
                        </React.StrictMode>);
 
 function HubbleTestUI () {
-  return     <iframe src="//hubble-ui-127-0-0-1.nip.io:8080/"
-               sandbox="allow-same-origin allow-scripts" />;
+  return <Split direction="vertical" minSize={1} className="split-vertical" sizes={[20, 80]}>
+           <TopControls />
+           <div>
+             <Split direction="horizontal" className="split-horizontal" sizes={[30, 70]}>
+               <LeftControls />
+               <iframe src="//hubble-ui-127-0-0-1.nip.io:8080/"
+               sandbox="allow-same-origin allow-scripts" />
+             </Split>
+           </div>
+         </Split>;
+}
+
+function TopControls () {
+  return <div></div>;
+}
+
+function LeftControls () {
+  return <div></div>;
 }
